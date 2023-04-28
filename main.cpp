d0b3c3e2bc7931c461353bb7350cf6ef877d9c1e
Victor Sidnei Cotta <111256513+victorcotta@users.noreply.github.com>
2023-04-28 16:13:13 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,51 +4,97 @@
 #include <string.h>
 
 //Definição dos pinos:
-#define encoder_A   2
-#define encoder_B   3
-#define IN1         5
-#define IN2         6
-#define ENA         9
+#define CLK              2
+#define SDA              3
+#define IN1              5
+#define IN2              6
+#define ENA              9
 
 //Resolução do encoder:
-#define resolution  12
+#define resolution       12
 
 //Razão de redução:
-#define reduction   298
+#define reduction_ratio  1/298
 
-//Raio da roda (em metros):
-#define raio        0.03
+//Raio da roda (em milímetros):
+#define raio             30
 
 //Definição de variáveis:
-bool active = false, direction_motor;
-int value = 0, val;
-long pulses = 0;
-unsigned long tempo_0 = 0, tempo_amostra = 1000;
-float v = 0, v_ang = 0;
+bool active = false;
+int value = 0;
+long pulses = 0, last_pulses = 0;
+unsigned long tempo_0 = 1000, tempo_ensaio = 1000, pined_time = 0, i = 0, t_ref = 0, ts = 50;
+float v = 0, v_ang = 0, pos[2], time[2];
 
-
-void count_pulses()
-{
-  pulses++;
-} //end count_pulses
-
-void update_velocity()
-{
-  v_ang = (((2*M_PI*pulses)/(reduction*(resolution/2)*tempo_amostra)));
-}
 
 //Inicializações:
 void setup() 
 {
   Serial.begin(115200);
 
-  pinMode(encoder_A, INPUT);
-  pinMode(encoder_B, INPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(encoder_A), count_pulses, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CLK), count_pulses, CHANGE);
+}
+
+void count_pulses()
+{
+  if(digitalRead(SDA))
+  {
+    pulses--;
+  }
+  else
+  {
+    pulses++;
+  }  
+}
+
+void update_velocity()
+{
+  switch(i%5)
+  {
+    case 0:
+      last_pulses = pulses;
+      i++;
+      t_ref = millis();
+    break;
+    case 1:
+      if((millis() - t_ref) >= ts)
+      {
+        i++;
+      }
+    break;
+    case 2:
+      if(pulses != last_pulses)
+      {
+        pos[0] = ((float)pulses*2*M_PI*reduction_ratio/(resolution/2.0));
+        time[0] = ((float)millis()/1e3);
+        last_pulses = pulses;
+        i++;
+        t_ref = millis();
+      }      
+    break;
+    case 3:
+      if((millis() - t_ref) >= ts)
+      {
+        i++;
+      }
+    break;
+    case 4:
+      if(pulses != last_pulses)
+      {
+        pos[1] = ((float)pulses*2*M_PI*reduction_ratio/(resolution/2.0));
+        time[1] = ((float)millis()/1e3);
+        v_ang = ((float)(pos[1]-pos[0])/(time[1]-time[0]));
+        v = v_ang*raio;
+        i++;
+      }
+    break;
+  }  
 }
 
 void loop() 
@@ -57,7 +103,7 @@ void loop()
   if(active)
   {
     //Retorna a velocidade atual:
-    Serial.println(v_ang);
+    Serial.println(v);
   }
 
   //Obtenção do comando de controle:
@@ -73,7 +119,7 @@ void loop()
     {
       value = round((value-150)*2.55);
     }    
-  }  
+  } 
 
   //Envio do sinal para o motor:
   if(value > 0)
@@ -93,6 +139,6 @@ void loop()
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, HIGH);
     analogWrite(ENA, 255);
-    v_ang = 0;
+    v = 0;
   }
 }
