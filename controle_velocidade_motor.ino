37d727f9bb7de48c16039914281dcdaa7b7dc18a
Victor Sidnei Cotta <111256513+victorcotta@users.noreply.github.com>
2023-07-13 11:51:10 -0300

    controle_velocidade_motor.ino

--- controle_velocidade_motor.ino
+++ controle_velocidade_motor.ino
@@ -1,6 +1,5 @@
 #include <math.h>
 
-
 #define    CLK              2                     //Conexão A do encoder
 #define    SDA              3                     //Conexão B do encoder
 #define    ENA              9                     //Saída PWM para controle de velocidade do motor
@@ -14,9 +13,9 @@
 
 
 volatile unsigned long pulses = 0;
-unsigned long tempo = 0, tempo_amostra = 1000;
-float v = 0, v_ang = 0, v_ref = 0;
-int PWM = 0;
+unsigned long tempo = 0, tempo_amostra = 1000, t0 = 0;
+float v = 0, v_ang = 0, v_ref;
+int pwm = 0;
 
 
 void setup()
@@ -26,38 +25,75 @@ void setup()
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(CLK), interrupcao, RISING);
-  Serial.begin(9600);
+  Serial.begin(2000000);
+  v_ref = 0;
 }
 
 void loop()
 {
-  v_ref = 5.5;
-
-  pwm = round(4.9469*pow(v_ref, 3) - 69.8478*pow(v_ref, 2) + 324.9863*pow(v_ref, 1) - 440.7973);
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  analogWrite(ENA, pwm);
-
-  if((millis() - tempo) >= 1000)
+  if(Serial.available())
   {
-    tempo = millis();
+    String reading = Serial.readString();
+    v_ref = reading.toFloat();
 
-    Serial.print(pulses);
+    if(((fabs(v_ref) < 2.8) || (fabs(v_ref) > 8.12)) && (v_ref != 0))
+    {
+      v_ref = 0;
+      Serial.println("\n Valor inválido!");
+    }
+    if(v_ref > 0)
+    {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+    }else
+    {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+    }
+    if(v_ref)
+    {
+      pwm = round(4.9469*pow(fabs(v_ref), 3) - 69.8478*pow(fabs(v_ref), 2) + 324.9863*pow(fabs(v_ref), 1) - 440.7973);
+    }
+    else
+    {
+      pwm = 0;
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+    }
+    analogWrite(ENA, pwm);
 
-    v_ang = (((float)(2*M_PI*pulses))/(reduction*(resolution/4.0)*(tempo_amostra/1000.0)));
-    v = v_ang*raio;
-    
-    Serial.print(" | ");
-    Serial.print("Velocidade angular (rad/s): ");
-    Serial.print(v_ang);
-   
-    Serial.print(" | ");
-    Serial.print("Velocidade linear (mm/s): ");
-    Serial.println(v);
-    pulses = 0;
+    if(v_ref)
+    {
+      t0 = micros();
+      do
+      {
+        if((micros() - tempo) >= tempo_amostra)
+        {
+          v_ang = (((float)(2*M_PI*pulses))/(reduction*(resolution/4.0)*((float)tempo_amostra/100000.0)));
+          v = v_ang*raio;
+          
+          Serial.print("Velocidade angular (rad/s): ");
+          Serial.print(v_ang);
+        
+          Serial.print(" | ");
+          Serial.print("Velocidade linear (mm/s): ");
+          Serial.println(v);
+          pulses = 0;
+
+          tempo = micros();
+        }
+      }while((micros() - t0) < (0.2*1e6)); //duração da amostragem
+    }
   }
+  v_ref = 0;
+  analogWrite(ENA, 0);
 }
+
+void interrupcao()
+{
+  pulses++;
+}
+
 
 void interrupcao()
 {
